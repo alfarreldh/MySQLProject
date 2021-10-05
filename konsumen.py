a018353a0f6a7c5c849c785fe85223c901373107
alfarreldh <80042014+alfarreldh@users.noreply.github.com>
2021-10-05 21:33:53 +0700

    Update konsumen.py

--- konsumen.py
+++ konsumen.py
@@ -1,7 +1,50 @@
 from flask import Flask, render_template, request, abort, make_response, jsonify, json
+import datetime,jwt
 from flask.wrappers import Response
 from flask_mysqldb import MySQL
+from functools import wraps
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'thisisthesecretkey'
+
+def token_required(f):
+    wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token') #http://127.0.0.1:5000/route?token=asdboin2i3oi2
+
+        if not token:
+            return json({'message' : 'Token is missing!'}), 403
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            return jsonify({'message' : 'Token is Invalid!'}), 403
+        
+        return f(*args, **kwargs)
+
+    return decorated
+
+@app.route('/unprotected')
+def unprotected():
+    return jsonify({'message': 'Anyone can view this!'})
+
+@app.route('/protected')
+@token_required
+def protected():
+    return jsonify({'message': 'Data bisa diakses'})
+
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if auth and auth.password == 'test':
+        token = jwt.encode({'user' : auth.username, 'exp' :datetime.datetime.utcnow() + datetime.timedelta(minutes=10)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token})
+
+    return make_response('Invalid',401,{ 'WWW-Authenticate' : 'Basic realm="Login Required"'})
+
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
